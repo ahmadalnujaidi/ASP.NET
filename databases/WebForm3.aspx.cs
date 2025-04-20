1fe710988bd8d7894b68559d47ea8f3bb5e3e913
ahmadalnujaidi <ahmadalnujaidi02@gmail.com>
2025-04-20 21:19:13 +0300

    finished ADO.NET

---
+++ databases/WebForm3.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace databases
+{
+    public partial class WebForm3 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection("data source = .; database = Sample; integrated security= SSPI"))
+            {
+                //SqlCommand cmd = new SqlCommand("select ProductId, ProductName, Description, Price, Quantity from tblProducts", con); executeReader
+
+                //SqlCommand cmd = new SqlCommand("select count(productId) from tblProducts", con); // executreScalar
+
+                //SqlCommand cmd = new SqlCommand("insert into tblProducts values (4, 'Apple watch', 'Appleshit', 100.90, 250)", con); // insert into table executeNonQuery
+
+                //SqlCommand cmd = new SqlCommand("update tblProducts set Quantity = 200 where ProductID = 1", con); // update table executeNonQuery
+
+                SqlCommand cmd = new SqlCommand("delete from tblProducts where ProductID = 3", con); // DELETE from table executeNonQuery
+
+                con.Open();
+                //GridView1.DataSource = cmd.ExecuteReader();
+                //GridView1.DataBind();
+
+                //int totalRows = (int)cmd.ExecuteScalar();
+                //Response.Write(totalRows);
+
+                int affectedRows = cmd.ExecuteNonQuery();
+                Response.Write(affectedRows);
+
+            }
+        }
+    }
+}
---
+++ databases/WebForm4.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace databases
+{
+    public partial class WebForm4 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string cs = "data source = .; database = Sample; integrated security = SSPI";
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                //string command = "Select * from tblProducts where ProductName like '" + TextBox1.Text + "%'";
+                //SqlCommand cmd = new SqlCommand(command, con);
+                // COULD CAUSE SQL INJECTION!
+                // for example inserting ip' delete from tblProducts --
+
+                // USING PARAMETRISED QUERIES
+                SqlCommand cmd = new SqlCommand("Select * from tblProducts where ProductName like @ProductName", con);
+                cmd.Parameters.AddWithValue("@ProductName", TextBox1.Text + "%");
+
+                // OR USING STORED PROCEDURES
+
+                
+                con.Open();
+                GridView1.DataSource = cmd.ExecuteReader();
+                GridView1.DataBind();
+                
+            }
+        }
+    }
+}
